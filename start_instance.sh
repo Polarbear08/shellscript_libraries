47f678af36897e5e17658b61d0ff6a9f1afe7d0d
polarbear08 <pistolstar0808@gmail.com>
2020-09-22 13:47:42 +0900

    [upd] modefied jenkins connection setupper

--- start_instance.sh
+++ start_instance.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-set -eux
+set -eu
 
 # check if the instance-id(argument) is given
-: $1
+: $1 $2 $3 $4
+
+# logging
+function log() {
+	: $1 $2
+	local level=$1
+	local msg=$2
+	if [ ${level} == "ERROR" ]; then
+		echo "[$(date +'%Y/%m/%d %T%:z')]${level} ${msg}" 1>&2
+	else
+		echo "[$(date +'%Y/%m/%d %T%:z')]${level} ${msg}"
+	fi
+}
 
 # get status of target instance
 function get_instance_state() {
@@ -16,28 +28,35 @@ function get_instance_state() {
 	
 	# exit if failed to get instance status
 	if [ -z ${status} ]; then
-		echo "failed to get status" 1>&2
+		log ERROR "failed to get status"
 		exit 1
 	else
-		echo ${status}
+		log INFO "current instance status: ${status}"
 	fi
 }
 
 # execute main procedure
 instance_id=$1
-status=`get_instance_state $1`
+private_key=$2
+user_name=$3
+host_name=$4
+status=`get_instance_state $1 | awk '{ print $NF }'`
 
 case ${status} in
 	"pending" )
-		echo "${instance_id} is now starting. wait a minute and retry."
+		log INFO "${instance_id} is now starting. wait a minute and retry."
 		sleep 60
 		bash $0 $1
 		;;
 	"running" )
-		echo "connect to ${instance_id}..."
-		ssh -i /root/.ssh/polaris.pem centos@34.202.215.167 -R 18080:localhost:8080 -Nf \
-			&& echo "completed!!" \
-			|| echo "failed to connect" 1>&2; exit 1
+		log INFO "connect to ${instance_id}..."
+		if [ `ps aux | grep 18080:localhost:8080 | wc -l` -gt 1 ]; then
+			log INFO "the connection has been already established"
+		else
+			ssh -i ${private_key} ${user_name}@${host_name} -R 18080:localhost:8080 -Nf \
+				&& (log INFO "completed!!") \
+				|| (log ERROR "failed to connect" 1>&2; exit 1)
+		fi
 		;;
 	"shutting-down" | "terminated" )
 		echo "the instance ${instance_id} has been terminated." 1>&2
